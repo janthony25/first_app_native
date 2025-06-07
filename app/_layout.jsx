8bdca6a1222d93829c7a058ae68a2a27dcc8a0d2
Jun Anthony <junanthony.business@gmail.com>
2025-06-07 21:40:16 +1200

    Logo changing and Spacer

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,28 +1,30 @@
-import { StyleSheet, Text, useColorScheme, View } from 'react-native'
-import { Link, Slot } from 'expo-router'
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { Colors } from '../constans/Colors';
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import { Link, Slot } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Colors } from "../constants/Colors";
 
 const RootLayout = () => {
-    const colorScheme = useColorScheme();
-    const theme = Colors[colorScheme] ?? Colors.light;
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light;
 
   return (
-    <View style={{flex: 1}}>
-      <Slot  />
+    <View style={{ flex: 1 }}>
+      <Slot />
 
-      <View style={[styles.footer, {backgroundColor: theme.background}]}>
-        <Link href="/"> <Ionicons name="home" size={30} color="#00BFFF" /></Link>
+      <View style={[styles.footer, { backgroundColor: theme.background }]}>
+        <Link href="/">
+          {" "}
+          <Ionicons name="home" size={30} color="#00BFFF" />
+        </Link>
       </View>
-      
     </View>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
 
 const styles = StyleSheet.create({
-    footer: {
-        padding: 20
-    }
-})
+  footer: {
+    padding: 20,
+  },
+});
--- app/index.jsx
+++ app/index.jsx
@@ -1,36 +1,45 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import { Link } from 'expo-router'
-import Logo from '../assets/img/logo_light.png'
+import { StyleSheet, Text } from "react-native";
+import { Link } from "expo-router";
+import Logo from "../assets/img/logo_light.png";
+
+import ThemedView from "../components/ThemedView";
+import ThemedLogo from "../components/ThemedLogo";
+import Spacer from "../components/Spacer";
 
 const Home = () => {
   return (
-    <View style={styles.container}>
+    <ThemedView style={styles.container}>
+      <ThemedLogo />
 
-      <Image source={Logo} />
-
+      <Spacer height={10} />
       <Text style={styles.title}>Welcome to Shelfie App!</Text>
-      <Link href="/about" style={styles.link}>About</Link>
-      <Link href="/contact" style={styles.link}>Contact</Link>
-    </View>
-  )
-}
+      <Spacer />
+      <Link href="/about" style={styles.link}>
+        About
+      </Link>
+      <Link href="/contact" style={styles.link}>
+        Contact
+      </Link>
+    </ThemedView>
+  );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    title: {
-        fontWeight: 'bold',
-        fontSize: 20
-    },
-    link: {
-        borderBottomWidth: 1,
-        fontSize: 16,
-        marginVertical: 10,
-        fontWeight: 'bold'
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  link: {
+    borderBottomWidth: 1,
+    fontSize: 16,
+    marginVertical: 10,
+    fontWeight: "bold",
+  },
+});
